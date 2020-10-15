68c9ee3c64215ad4a3492c82147f0906ef638764
Woloda <dorozovecvoloda@gmail.com>
2020-10-15 21:31:40 +0300

    Lab__3.4

---
+++ Lab_3.4/Lab_3.4.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <cmath>
+#include <Windows.h>
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	double x;
+	double y;
+	double R;
+
+	cout << "x = ";  cin >> x;
+	cout << endl;
+	cout << "y = ";  cin >> y;
+	cout << endl;
+	cout << "R = ";  cin >> R;
+	cout << endl;
+
+
+	if (y >= sqrt(R * R - (pow(x + R, 2))) - R && y <= 0 && x >= -R)
+		cout << "Так1" << endl;
+	else
+		cout << "Ні1" << endl;
+	if (y <= sqrt(R * R - (pow(x - R, 2))) && y >= 0)
+		cout << "Так2" << endl;
+	else
+		cout << "Ні2" << endl;
+
+	cin.get();
+	return 0;
+}
